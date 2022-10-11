408930f92eb085ba890bba1e0577cd30bd6cb48a
Deyanira Genao <deyagenao@gmail.com>
2022-10-11 15:02:28 -0400

    initial commit

---
+++ JenkinsDemo/src/main/java/com/jenkins/HelloJenkins.java
@@ -0,0 +1,17 @@
+package com.jenkins;
+
+public class HelloJenkins {
+
+	public static void main(String[] args) {
+		System.out.println(helloJenkins());
+		System.out.println(helloJenkinsAgain());
+	}
+	
+	public static String helloJenkins() {
+		return "Hello Jenkins!";
+	}
+	
+	public static String helloJenkinsAgain() {
+		return "Hello Jenkins again!!";
+	}
+}
